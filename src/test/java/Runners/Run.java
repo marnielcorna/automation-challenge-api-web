9b71b9fe65558049ae1110284f7345bfa218afe6
Marcos Daniel <marcoscortezqatesting@outlook.com>
2023-03-12 17:34:07 +0100

    demo report

--- src/test/java/Runners/Run.java
+++ src/test/java/Runners/Run.java
@@ -1,6 +1,14 @@
 package Runners;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import net.masterthought.cucumber.Configuration;
+import net.masterthought.cucumber.ReportBuilder;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.AfterClass;
+import net.masterthought.cucumber.Configuration;
 
 @CucumberOptions(
         features = {"src/test/resources/features/api/apitest.feature","src/test/resources/features/web/webtest.feature"},
@@ -10,4 +18,20 @@ import io.cucumber.testng.CucumberOptions;
 )
 public class Run extends AbstractTestNGCucumberTests {
 
+    @AfterClass
+    public static void generateReport() {
+        File reportOutputDirectory = new File("target");
+        List<String> jsonFiles = new ArrayList<>();
+        jsonFiles.add("target/cucumber-json-report.json");
+        String projectName = "My Project";
+
+        Configuration configuration = new Configuration(reportOutputDirectory, projectName);
+        configuration.addClassification("Environment", "DEV");
+        configuration.addClassification("Browser", "Chrome");
+        configuration.addClassification("Branch", "master");
+        configuration.addClassification("Build Number", "1234");
+
+        ReportBuilder reportBuilder = new ReportBuilder(jsonFiles, configuration);
+        reportBuilder.generateReports();
+    }
 }
